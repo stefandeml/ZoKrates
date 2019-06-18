f1512c60aa01b570c4a49d38fcdfd71e3807522a
sdeml <stefandeml@gmail.com>
2019-06-18 19:19:02 +0200

    add run script

---
+++ demo/run.sh
@@ -0,0 +1,6 @@
+set -e
+../target/release/zokrates compile -i nft.code --light
+../target/release/zokrates compute-witness -a $(cat nft_witness.txt)
+../target/release/zokrates setup
+../target/release/zokrates export-verifier
+../target/release/zokrates generate-proof
