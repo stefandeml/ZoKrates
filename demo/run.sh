616bc1884c95094cff9188951c30b63208bd563d
Miguel Hervas <miguel.hervas.lazaro@gmail.com>
2019-06-21 19:07:58 +0200

    last changes

--- demo/run.sh
+++ demo/run.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+
 set -e
-../target/release/zokrates compile -i nft.code --light
+#../target/release/zokrates compile -i nft.code --light
 ../target/release/zokrates compute-witness -a $(cat nft_witness.txt)
-../target/release/zokrates setup
-../target/release/zokrates export-verifier
+#../target/release/zokrates setup
+#../target/release/zokrates export-verifier
 ../target/release/zokrates generate-proof
